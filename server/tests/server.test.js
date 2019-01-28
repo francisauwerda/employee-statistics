67e36ab309752b2f4dd0dde56e36f40340206411
Francis Auwerda <fauwerda@gmail.com>
2019-01-28 06:30:24 +0100

    Created a test for POST /api/employees

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,7 +5,7 @@ const request = require('supertest');
 const app = require('./../../app');
 const { Employee } = require('./../models');
 
-const employees = [{
+const initialTestEmployees = [{
   firstName: 'testFirst',
   lastName: 'testLast',
   department: 'testDepartment',
@@ -20,11 +20,11 @@ const employees = [{
 beforeEach((done) => {
   Employee
     .destroy({ where: {} })
-    .then(() => Employee.bulkCreate(employees))
+    .then(() => Employee.bulkCreate(initialTestEmployees))
     .then(() => done());
 });
 
-describe('GET /employees', () => {
+describe('GET /api/employees', () => {
   it('should get all employees', (done) => {
     request(app)
       .get('/api/employees')
@@ -33,5 +33,42 @@ describe('GET /employees', () => {
         expect(res.body.employees.length).toBe(2);
       })
       .end(done);
+  });
+});
+
+describe('POST /api/employees', () => {
+  it('should create an employee', (done) => {
+    const body = {
+      firstName: 'Michael',
+      lastName: 'Jackson',
+      department: 'Music',
+      nationality: 'American',
+    };
+
+    request(app)
+      .post('/api/employees')
+      .send(body)
+      .expect(201)
+      .expect((res) => {
+        expect(res.body.firstName).toBe(body.firstName);
+      })
+      .end((err) => {
+        if (err) {
+          return done(err);
+        }
+
+        return Employee
+          .findAll({
+            where: {
+              firstName: body.firstName,
+            },
+          })
+          .then((employees) => {
+            expect(employees.length).toBe(1);
+            expect(employees[0].lastName).toBe(body.lastName);
+            done();
+          })
+          .catch(e => done(e));
+      });
   });
 });
